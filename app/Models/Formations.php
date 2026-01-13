f88c8d738a1390ade22ed899a8240d6a2692fd2c
Mathis Mollier <superuchiwa39@gmail.com>
2026-01-13 17:08:52 +0100

    feat(change_models_participant_entreprise_formation) : Changer les models pour ajouter  les fillables et les relations

--- app/Models/Formations.php
+++ app/Models/Formations.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Formations extends Model
 {
-    //
+    protected $fillable = [
+        "libelle"
+    ];
+    public function contrats() : HasMany
+    {
+        return $this->hasMany(Contrat::class);
+    }
+    public function ecoles() : BelongsToMany
+    {
+        return $this->belongsToMany(Ecole::class);
+    }
 }
