be2677ba39fe3a7e040faa80baa78454fe618948
melanie <mtroutier@gtoupemontroland.fr>
2026-03-31 17:07:03 +0200

    feat(participation) : renvoi donnéesfront

--- resources/js/Pages/Surveys/Participants/Fill.tsx
+++ resources/js/Pages/Surveys/Participants/Fill.tsx
@@ -9,5 +9,18 @@ interface Props {
 }
 
 export default function Fill(data: Props) {
-    return <div>{data.prenom}</div>;
+    return (
+        <>
+            <div>{data.nom}</div>
+            <div>{data.prenom}</div>
+            <div>{data.telephone}</div>
+            <div>{data.mail}</div>
+            <div>{data.enquete.titre}</div>
+            <div>{data.enquete.description}</div>
+            <div>{data.enquete.date_debut}</div>
+            <div>{data.enquete.date_fin}</div>
+            <div>{data.enquete.type_campagne}</div>
+          
+        </>
+    );
 }
