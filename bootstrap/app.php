c66e1a87d5c7890ce90e6c8e888e5c2df868ea85
Yulian <105125828+YulianGuinand@users.noreply.github.com>
2026-04-26 15:13:41 +0200

    fix: online

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,6 +25,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'logout',
             'register',
         ]);
+        $middleware->trustProxies(at: '*');
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
